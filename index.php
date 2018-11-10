d06c9a7e7361042e19f3475a9e160f3416bdfeaf
divannaru <43481157+divannaru@users.noreply.github.com>
2018-11-10 19:23:32 +0300

    Update index.php
    добавлен код второго калькулятора

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <title>Калькулятор</title>
     <style>
-        div {
+        div, h1 {
             text-align: center;
         }
 
@@ -11,7 +11,9 @@
 </head>
 
 <body>
+    <h1>Калькулятор</h1>
     <div>
+      <h2>Первый вариант</h2>
         <form action="" method="POST" enctype="multipart/form-data">
             <p>Введите первое число</p>
             <input type="text" name="number1"><br>
@@ -20,20 +22,40 @@
             <br>
             <p><select size="3" multiple name="calculate">
                 <option disabled>Выберите арифметическое действие</option>
-                <option selected value="Сложение">Сложение</option>
-                <option value="Вычитание">Вычитание</option>
-                <option value="Умножение">Умножение</option>
-                <option value="Деление">Деление</option>
+                <option selected value="Сложить">Сложить</option>
+                <option value="Вычесть">Вычесть</option>
+                <option value="Умножить">Умножить</option>
+                <option value="Разделить">Разделить</option>
             </select></p>
             <input type="submit" name="submit" value="Вычислить">
         </form>
         <?
             if (isset($_POST["submit"])){
-                include "calculate.php";
+                include "calculatetest.php";
             }
         ?>
-<p>Результат операции: <?=$res?></p>
-
+        <p>Результат операции: <?=$res?></p>
+    </div>
+    <hr>
+    <div>
+       <h2>Второй вариант</h2>
+        <form action="" method="POST" enctype="multipart/form-data">
+            <p>Введите первое число</p>
+            <input type="text" name="number3"><br>
+            <p>Введите второе число</p>
+            <input type="text" name="number4"><br>
+            <p>Выберите арифметическое действие</p>
+            <input type="submit" name="submit2" value="Сложить">
+            <input type="submit" name="submit2" value="Вычесть">
+            <input type="submit" name="submit2" value="Умножить">
+            <input type="submit" name="submit2" value="Разделить">
+        </form>
+        <?
+            if ($_POST["submit2"]!=null){
+                include "calculate2.php";
+            }
+        ?>
+        <p>Результат операции: <?=$res1?></p>
     </div>
 </body>
 
